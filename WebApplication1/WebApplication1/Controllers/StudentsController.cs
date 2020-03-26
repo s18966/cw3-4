565f7446657171e3688f81ec39f06e3e9770f66b
s18966 <61682260+s18966@users.noreply.github.com>
2020-03-26 03:16:00 +0100

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.DAL;
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDBService _dBService;
+        public StudentsController(IDBService dBService)
+        {
+            _dBService = dBService;
+        }
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(_dBService.GetStudents());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //..add to db
+            //..generate index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudents(int id)
+        {
+            {
+                return Ok("Updated");
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudents(int id)
+        {
+            {
+                return Ok("Deleted");
+            }
+        }
+
+    }
+}
